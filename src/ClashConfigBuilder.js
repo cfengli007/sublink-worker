f066c4d559ab5029f843b4d8e5d3fdf29e108e50
cfengli007 <cf192955@outlook.com>
2025-05-28 10:39:44 +0800

    fix: 移除节点选择组中多余的新加坡自动选择项
    原代码在代理列表中添加了多余的"Singapore Auto Select"选项，这可能导致用户混淆。现移除该选项以保持节点选择组的简洁性。

--- src/ClashConfigBuilder.js
+++ src/ClashConfigBuilder.js
@@ -203,7 +203,7 @@ export class ClashConfigBuilder extends BaseConfigBuilder {
     }
 
     addNodeSelectGroup(proxyList) {
-        proxyList.unshift('DIRECT', 'REJECT', t('outboundNames.Auto Select'), t('outboundNames.Singapore Auto Select'));
+        proxyList.unshift('DIRECT', 'REJECT', t('outboundNames.Auto Select'));
         this.config['proxy-groups'].unshift({
             type: "select",
             name: t('outboundNames.Node Select'),
