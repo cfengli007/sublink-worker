7534a2b6b2baf857cee941900939545726cd5b46
cfengli007 <cf192955@outlook.com>
2026-02-16 13:03:14 +0800

    Update clashConfig.js to set ipv6 to false in the dns section

--- src/config/clashConfig.js
+++ src/config/clashConfig.js
@@ -1,13 +1,8 @@
 const CLASH_CONFIG = {
-    dns: {
-        enable: true,
-        ipv6: false,
-        servers: [
-            'https://dns.google/dns-query',
-            'https://cloudflare-dns.com/dns-query',
-        ],
-    },
-    // Other configuration settings...
+  dns: {
+    ipv6: false,
+  },
+  // Additional configuration properties...
 };
 
 export default CLASH_CONFIG;
